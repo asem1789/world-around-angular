44ff33e4f95e7b435874fe8199884cf4532d893f
asem1789 <as.1789@hotmail.com>
2021-03-10 12:58:40 +0200

    colors variables, with theme service, #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './services';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'world-around-angular';
+
+  constructor(private themeService: ThemeService){}
+
+  toggleTheme() {
+    if(this.themeService.isDarkMode()){
+      this.themeService.setActiveMode('light');
+    } else {
+      this.themeService.setActiveMode('dark');
+    }
+  }
+
 }
