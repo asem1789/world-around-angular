aecd45451e2d3c2ca9e2807c364eb9d13a881730
asem1789 <as.1789@hotmail.com>
2021-03-17 08:38:19 +0200

    handle filterByRegion by queryParms

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { CountryInfo } from 'src/app/models';
 import { CountriesService } from 'src/app/services/countries.service';
 
@@ -10,31 +11,69 @@ import { CountriesService } from 'src/app/services/countries.service';
 export class HomePageComponent implements OnInit {
   countries$: CountryInfo[] = [];
   searchText: string = '';
+  searchRegion: string = '';
 
-  constructor(private countriesService: CountriesService) {}
+  constructor(
+    private countriesService: CountriesService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.countriesService.getAllCountries().subscribe((res: CountryInfo[]) => {
-      this.countries$ = res;
-      // this.copyCountries = res;
-      /*
-       * try to collect Palestine and Israeli occupation in only array
-       * in order handle them esaily
-       * when search is able to seach about isreal word and other Zionist occupation ...
-       */
+    this.route.queryParams.subscribe((params) => {
+      if (this.isParamsEmpty(params)) {
+        this.getAllCountries();
+      } else {
+        if (!!params.region && this.checkValidQuery(params.region)) {
+          this.getCountriesByRegion(params.region);
+        } else {
+          this.router.navigate(['/countries']);
+        }
+      }
     });
   }
 
-  searchingCountry(value: string) {
+  onSearchingCountry(value: string) {
     this.searchText = value;
   }
 
-  filterByRegion(value: string) {
+  onFilterByRegion(value: string) {
+    this.router.navigate(['/countries'], { queryParams: { region: value } });
+  }
+
+  getAllCountries() {
+    this.countriesService.getAllCountries().subscribe((res: CountryInfo[]) => {
+      this.countries$ = res;
+    });
+  }
+
+  getCountriesByRegion(region: string) {
     this.countriesService
-      .getCountriesByRegion(value)
+      .getCountriesByRegion(region)
       .subscribe((res: CountryInfo[]) => {
+        this.searchRegion = region;
         this.countries$ = res;
-        // this.copyCountries = res;
       });
+  }
+
+  isParamsEmpty(params: any): boolean {
+    if (Object.keys(params).length === 0 && params.constructor === Object) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  checkValidQuery(region: string): boolean {
+    if (
+      region === 'Africa' ||
+      region === 'Americas' ||
+      region === 'Asia' ||
+      region === 'Europe' ||
+      region === 'Oceania'
+    ) {
+      return true;
+    }
+    return false;
   }
 }
