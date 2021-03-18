9d143f65c3aed26c099ed103428b8cda86d4bfee
asem1789 <as.1789@hotmail.com>
2021-03-18 16:05:17 +0200

    handle darkmode with localStorage, #34

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'world-around-angular';
+
+  constructor(private themeService: ThemeService){}
+
+  ngOnInit() {
+    const mode = localStorage.getItem('active-mode');
+    if(mode === 'light' || null) {
+      this.themeService.setActiveMode('light')
+    }
+    else if(mode === 'dark') {
+      this.themeService.setActiveMode('dark');
+    }
+  }
 
  
 }
