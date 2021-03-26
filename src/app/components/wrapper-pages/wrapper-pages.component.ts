643691420ede29f80e3b529fd26d964bc7f0780f
asem1789 <as.1789@hotmail.com>
2021-03-26 14:13:50 +0200

    make component as wrapper pages insted using the same styling and the same dealing with media screen

---
+++ src/app/components/wrapper-pages/wrapper-pages.component.ts
@@ -0,0 +1,12 @@
+/*
+ * this component for handling padding with all screen width
+ * instead copy the same classes with all pages
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wrapper-pages',
+  templateUrl: './wrapper-pages.component.html',
+  styleUrls: ['./wrapper-pages.component.css'],
+})
+export class WrapperPagesComponent {}
