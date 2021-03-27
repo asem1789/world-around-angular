115c5b47e46483a0a0f9d36a9c698e6bc03d31d0
asem1789 <as.1789@hotmail.com>
2021-03-27 13:24:41 +0300

    delete pages and undo last merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailPageComponent } from './pages/detail-page/detail-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NotFoundComponent } from './pages/not-found-page/not-found.component';
-import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 export const routes: Routes = [
   // {path: "", component: HomePageComponent},
   { path: '', redirectTo: '/countries', pathMatch: 'full' },
   { path: 'countries', component: HomePageComponent },
   { path: 'countries/:countryName/detail', component: DetailPageComponent },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'signup', component: SignupPageComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
