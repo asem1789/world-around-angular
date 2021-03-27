df776a9026ba778c73c6fd4ec96a2466655a15ac
asem1789 <as.1789@hotmail.com>
2021-03-27 17:06:45 +0300

    using integration tests by cypress and update readme

--- cypress/integration/detail-page.component.spec.js
+++ cypress/integration/detail-page.component.spec.js
@@ -1,5 +1,12 @@
-describe("Detail page tests", () => {
-  it("should fist the next broder countries when click on broder", () => {
-    cy.visit("/countries/Afghanistan/detail").get(".border").first().click();
+describe("Detail_Page Integration tests", () => {
+  it("Should go to the new countries when click on border, And go back when click on back button", () => {
+    cy.visit("/countries/Afghanistan/detail")
+      .get(".border")
+      .first()
+      .click()
+      .get(".back")
+      .click()
+      .get(".back")
+      .click();
   });
 });
--- cypress/integration/home-page.component.spec.js
+++ cypress/integration/home-page.component.spec.js
@@ -1,18 +1,35 @@
-describe("our coutries app test suite", () => {
-  it("first test", () => {
-    expect(true).to.equal(true);
-  });
-
-  it("Visting our app", () => {
+describe("Home page testing with its process", () => {
+  it("Visting Home page, should have title `World AROUND`", () => {
     cy.visit("/");
     cy.get(".logo").contains("WORLD AROUND");
   });
 
-  it("Type title then click card then go to the detail page", () => {
+  it("Search by name should be Successful", () => {
     cy.visit("/")
       .get(".input-search")
       .type("Palesti")
       .get("app-card-country")
-      .click();
+      .contains("Palestine");
+  });
+
+  it("Filter By Region should work as expected", () => {
+    cy.visit("/")
+      .get(".select-input")
+      .click()
+      .get("[data-value=americas]")
+      .click()
+      .get("app-card-country")
+      .contains("United States of America");
+  });
+
+  it("Shuld navigate into detail page when click on Afghanistan cuntry", () => {
+    cy.visit("/").get("app-card-country").contains("Afghanistan").click();
+    cy.url().then(($url) => {
+      if ($url.includes("/detail")) {
+        cy.log("Success Naviage");
+      } else {
+        cy.log("No");
+      }
+    });
   });
 });
